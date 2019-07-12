477f2627df52abcccd1cb6e43ec81b818c95b900
Xavi <xavi@geekshubs.com>
2019-07-12 14:27:15 +0200

    Implemented devuelveLaSegundaColumanDeunSudokuDeOrden2 Test

--- src/test/java/com/javier/rodriguez/SudokuTDDEdicom/SudokuTEst.java
+++ src/test/java/com/javier/rodriguez/SudokuTDDEdicom/SudokuTEst.java
@@ -89,6 +89,15 @@ public class SudokuTEst {
 			int[] columna = sudoku.getColumna(0);
 			assertArrayEquals(new int[] {1,5,9,13}, columna);
 		}
+		@Test
+		public void devuelveLaSegundaColumanDeunSudokuDeOrden2() {
+			int orden =2;
+			Sudoku sudoku = new Sudoku(orden);
+			int [] datos = new int[] {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
+			sudoku.cargar(datos);
+			int[] columna = sudoku.getColumna(1);
+			assertArrayEquals(new int[] {2,6,10,14}, columna);
+		}
 }		
 
 

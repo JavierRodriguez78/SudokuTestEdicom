b093173fff696aeec3996d8faec619a91a17f15a
Xavi <xavi@geekshubs.com>
2019-07-12 13:39:56 +0200

    [RED] Implemented nSudokuDeOrden2TieneEnTotal16Casillas Test

---
+++ src/main/java/com/javier/rodriguez/SudokuTDDEdicom/Sudoku.java
@@ -0,0 +1,28 @@
+package com.javier.rodriguez.SudokuTDDEdicom;
+
+public class Sudoku {
+
+	
+
+
+	private final int orden;
+	
+	
+	public Sudoku(int orden) {
+		super();
+		this.orden = orden;
+	}
+
+
+
+	
+	
+	public int getOrden() {
+		return orden;
+	}
+	
+	public int getTotalCasillas() {
+		return 1;
+	}
+	
+}
--- src/test/java/com/javier/rodriguez/SudokuTDDEdicom/SudokuTEst.java
+++ src/test/java/com/javier/rodriguez/SudokuTDDEdicom/SudokuTEst.java
@@ -9,7 +9,7 @@ public class SudokuTEst {
 
 	@Test
 	public void prueba() {
-		assertTrue(false);
+		assertTrue(true);
 	}
 	
 	/**
@@ -20,6 +20,22 @@ public class SudokuTEst {
 		int orden = 1;
 		Sudoku sudoku = new Sudoku(orden);
 		int totalCasillas = sudoku.getTotalCasillas();
-		assertEquals((1, totalCasillas);
+		assertEquals(1, totalCasillas);
 	}
-}
+	/**
+	 * 1  2  1  2
+	 * 3  4  3  4 
+	 * 1  2  1   2
+	 * 3  4   2  4
+	 */
+		@Test
+		public void unSudokuDeOrden2TieneEnTotal16Casillas() {
+			int orden = 1;
+			Sudoku sudoku = new Sudoku(orden);
+			int totalCasillas = sudoku.getTotalCasillas();
+			assertEquals(16, totalCasillas);
+		}
+
+}		
+
+
